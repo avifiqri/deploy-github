443029f40aa729e146cd4d66b5fa9aae2b2b7ed8
avifiqri <myusufavifiqri@gmail.com>
2019-03-18 20:14:09 +0700

    Updates

---
+++ precache-manifest.6cfcb69604d204f362e1ee5e64d8a8b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2a705a7350a70d38def7684ade8c6e4",
+    "url": "/static/media/Gambar2.a2a705a7.jpeg"
+  },
+  {
+    "revision": "ff46e42354f43cea8bf3d0b6ab4fe9ed",
+    "url": "/static/media/Gambar1.ff46e423.jpeg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "35745b780873621ab553",
+    "url": "/static/js/main.b502f0d6.chunk.js"
+  },
+  {
+    "revision": "6c3ee1949a14ffbdfb24",
+    "url": "/static/js/2.0be8adc5.chunk.js"
+  },
+  {
+    "revision": "35745b780873621ab553",
+    "url": "/static/css/main.e58b6f17.chunk.css"
+  },
+  {
+    "revision": "11ff70ea33135ef6bb93478a615340c7",
+    "url": "/index.html"
+  }
+];
